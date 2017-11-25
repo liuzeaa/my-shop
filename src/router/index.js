c384f6113741a14fb0175a5d2d962edcf7c00c5c
liuzeaa <1098367891@qq.com>
2017-11-25 08:42:28 +0800

    修改 route 为按需加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-/*const GoodsList = ()=> import('@/views/GoodsList');
+const GoodsList = ()=> import('@/views/GoodsList');
 const Cart = ()=> import('@/views/Cart');
 const Address = ()=> import('@/views/Address');
 const OrderConfirm = ()=> import('@/views/OrderConfirm');
 const OrderSuccess = ()=> import('@/views/OrderSuccess');
 const OrderList = ()=> import('@/views/OrderList');
-const OrderItem = ()=> import('@/views/OrderItem');*/
-import GoodsList from '@/views/GoodsList.vue';
+const OrderItem = ()=> import('@/views/OrderItem');
+/*import GoodsList from '@/views/GoodsList.vue';
 import Cart from '@/views/Cart';
 import Address from '@/views/Address'
 import OrderConfirm from '@/views/OrderConfirm'
 import OrderSuccess from '@/views/OrderSuccess'
 import OrderList from '@/views/OrderList'
-import OrderItem from '@/views/OrderItem'
+import OrderItem from '@/views/OrderItem'*/
 Vue.use(Router)
 
 export default new Router({
